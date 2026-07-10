854b61504c1bd348c707946d756cfda48c9655d3
Gonzalo Grassi <gonza.gr92@gmail.com>
2026-07-10 15:38:45 -0300

    Setup react query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,35 @@
 import "@/styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
+import {broadcastQueryClient} from '@tanstack/query-broadcast-client-experimental'
+
+const DEFAULT_STALE_TIME = 1000 * 60 * 5 // 5 minutes
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // refetchOnWindowFocus: false,
+            staleTime: DEFAULT_STALE_TIME,
+          },
+        },
+      }),
+  )
+
+    useEffect(() => {
+      return broadcastQueryClient({
+        queryClient,
+        broadcastChannel: 'tanstack-query',
+      })
+    }, [queryClient])
+
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />;
+    </QueryClientProvider>
+  );
 }
