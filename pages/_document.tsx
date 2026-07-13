40913353a8ca7d86b37c4cdbe087e855c2621065
Gonzalo Grassi <gonza.gr92@gmail.com>
2026-07-13 14:53:37 -0300

    Fix basepath for favicon, change solution

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
-import { addBasePath } from 'next/dist/client/add-base-path'
 import { Head, Html, Main, NextScript } from 'next/document'
+
+const basePath = process.env.PAGES_BASE_PATH ?? ''
 
 export default function Document() {
   return (
@@ -11,10 +12,10 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
           rel="stylesheet"
         />
-        <link rel="apple-touch-icon" sizes="180x180" href={addBasePath('/apple-touch-icon.png')} />
-        <link rel="icon" type="image/png" sizes="32x32" href={addBasePath('/favicon-32x32.png')} />
-        <link rel="icon" type="image/png" sizes="16x16" href={addBasePath('/favicon-16x16.png')} />
-        <link rel="manifest" href={addBasePath('/site.webmanifest')} />
+        <link rel="apple-touch-icon" sizes="180x180" href={`${basePath}/apple-touch-icon.png`} />
+        <link rel="icon" type="image/png" sizes="32x32" href={`${basePath}/favicon-32x32.png`} />
+        <link rel="icon" type="image/png" sizes="16x16" href={`${basePath}/favicon-16x16.png`} />
+        <link rel="manifest" href={`${basePath}/site.webmanifest`} />
       </Head>
       <body>
         <Main />
