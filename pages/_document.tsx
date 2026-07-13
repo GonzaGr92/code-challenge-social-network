2240c03d4ad4f1b68ec6423d5997a07f2e241454
Gonzalo Grassi <gonza.gr92@gmail.com>
2026-07-13 14:48:26 -0300

    Fix basepath for favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+import { addBasePath } from 'next/dist/client/add-base-path'
 import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
@@ -10,10 +11,10 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
           rel="stylesheet"
         />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="apple-touch-icon" sizes="180x180" href={addBasePath('/apple-touch-icon.png')} />
+        <link rel="icon" type="image/png" sizes="32x32" href={addBasePath('/favicon-32x32.png')} />
+        <link rel="icon" type="image/png" sizes="16x16" href={addBasePath('/favicon-16x16.png')} />
+        <link rel="manifest" href={addBasePath('/site.webmanifest')} />
       </Head>
       <body>
         <Main />
